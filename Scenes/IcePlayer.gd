94c80ba49cd1b3dbb91050c3182a0e31b332b237
fhakim19 <faysalibrahimwork@gmail.com>
2021-11-25 18:34:58 +0000

    tried to add friction :skull:

--- Scenes/IcePlayer.gd
+++ Scenes/IcePlayer.gd
@@ -1,83 +1,30 @@
 extends KinematicBody2D
 
-export var jump_force = 0.0
-export var _speed = 0.0
-export var _gravity = 0.0
-export var can_jump_start = 0
+export (int) var speed = 150
+export (int) var jump_speed = -250
+export (int) var gravity = 250
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
 
-var can_jump = 0
+var velocity = Vector2.ZERO
 
-var _velocity = Vector2()
-var _floor = Vector2(0, -1)
+func get_input():
+	var dir = 0
+	velocity.x = 0
+	if Input.is_action_pressed("ui_right"):
+		dir += 1
+	if Input.is_action_pressed("ui_left"):
+		dir -= 1
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0, friction)
 
-
-
-enum {
-	IDLE
-	WALK
-	AIR
-}
-
-var _state: int = IDLE
-
-func flip_sprite() -> void:
-	if _velocity.x < 0:
-		$AnimatedSprite.flip_h = true
-	if move_direction().x > 0:
-		$AnimatedSprite.flip_h = false
-
-
-func _ready():
-	can_jump = can_jump_start
-	print(can_jump)
 
 func _physics_process(delta):
-	if is_on_floor() and _state == IDLE:
-		$AnimatedSprite.play("Idle")
-	flip_sprite()
-	var my_move_direction = move_direction()
-	_velocity.y += _gravity
-	
-	if is_on_floor():
-		can_jump = can_jump_start
-	
-	match _state:
-		IDLE:
-			_velocity.x = 0
-			if my_move_direction:
-				change_state(WALK)
-			elif Input.is_action_just_pressed("ui_up") and can_jump > 0:
-				_velocity.y = -jump_force
-				can_jump -= 1
-				change_state(AIR)
-		WALK:
-			$AnimatedSprite.play("Walk")
-			if not my_move_direction:
-				change_state(IDLE)
-			elif Input.is_action_just_pressed("ui_up") and can_jump > 0:
-				_velocity.y = -jump_force
-				can_jump -= 1
-				change_state(AIR)
-			else:
-				_velocity.x = my_move_direction.x * _speed
-		AIR:
-			$AnimatedSprite.play("Jump")
-			if my_move_direction:
-				_velocity.x = my_move_direction.x * _speed
-			else: 
-				_velocity.x = 0
-			
-			if _velocity.y > 1:
-				change_state(IDLE)
-			
-	_velocity = move_and_slide(_velocity, _floor)
-	
-
-func move_direction() -> Vector2:
-	return Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 0
-	)
-
-func change_state(target_state: int):
-	_state = target_state
-
+	get_input()
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	if Input.is_action_just_pressed("ui_up"):
+		if is_on_floor():
+			velocity.y = jump_speed
