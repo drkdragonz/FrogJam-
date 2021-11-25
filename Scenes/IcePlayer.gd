5dd3f98ee966db1b4b4f8b26812c618bba3313df
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-11-25 19:05:44 +0000

    Player script needs fix. movement adjusted

--- Scenes/IcePlayer.gd
+++ Scenes/IcePlayer.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
-export (int) var speed = 150
-export (int) var jump_speed = -250
-export (int) var gravity = 250
-export (float, 0, 1.0) var friction = 0.1
-export (float, 0, 1.0) var acceleration = 0.25
+export (int) var speed = 1200
+export (int) var jump_speed = -1800
+export (int) var gravity = 4000
+
+export var Jumps = 2
+var can_Jump = 0
 
 var velocity = Vector2.ZERO
 
+export (float, 0, 1.0) var friction = 0.1
+export (float, 0, 1.0) var acceleration = 0.25
+
 func get_input():
 	var dir = 0
-	velocity.x = 0
 	if Input.is_action_pressed("ui_right"):
 		dir += 1
 	if Input.is_action_pressed("ui_left"):
@@ -19,12 +22,17 @@ func get_input():
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
-
+func _ready():
+	can_Jump == Jumps
 
 func _physics_process(delta):
+	if is_on_floor():
+		can_Jump == Jumps
 	get_input()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 	if Input.is_action_just_pressed("ui_up"):
-		if is_on_floor():
+		if can_Jump > 0:
+			can_Jump -= 1
 			velocity.y = jump_speed
+
