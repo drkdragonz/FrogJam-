2ad082ec3cabb828aafc8d3878547d76e38a0ecf
fhakim19 <faysalibrahimwork@gmail.com>
2021-11-27 18:22:48 +0000

    ice player complete with double jump and animation fix

--- Scenes/IcePlayer.gd
+++ Scenes/IcePlayer.gd
@@ -4,8 +4,8 @@ export (int) var speed = 1200
 export (int) var jump_speed = -1800
 export (int) var gravity = 4000
 
-export var Jumps = 2
-export var can_Jump = 0
+var jumps = 0
+const max_jumps = 2
 
 var velocity = Vector2.ZERO
 
@@ -16,24 +16,25 @@ func get_input():
 	var dir = 0
 	if Input.is_action_pressed("ui_right"):
 		dir += 1
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Walk")
 	if Input.is_action_pressed("ui_left"):
 		dir -= 1
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Walk")
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
+		$AnimatedSprite.play("Idle")
 		velocity.x = lerp(velocity.x, 0, friction)
 
-func _ready():
-	can_Jump == Jumps
-
 func _physics_process(delta):
-	if is_on_floor():
-		can_Jump == Jumps
 	get_input()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("ui_up"):
-		if can_Jump > 0:
-			can_Jump -= 1
+	if jumps < max_jumps and Input.is_action_just_pressed("ui_up"):#
+			$AnimatedSprite.play("Jump")
 			velocity.y = jump_speed
-
+			jumps += 1
+	elif is_on_floor():
+		jumps = 0
