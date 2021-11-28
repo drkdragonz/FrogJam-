63798f7c8ff44eefe8f6d5b88d5a12646e2d9cf7
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-11-28 08:17:12 +0000

    Started Forest 1, Adjusted world 2 and adjusted icePlayer

---
+++ Scenes/World3 To ice1.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Fade Out":
+		get_tree().change_scene("res://Scenes/Ice1.tscn")
+
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		$AnimationPlayer.play("Fade Out")
