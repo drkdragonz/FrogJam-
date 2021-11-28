f966af180912dadc98486f4563f6987df5f440a4
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-11-28 17:47:31 +0000

    Finished Level 1 of the Forest

---
+++ Scenes/FallingPlatform.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		$AnimationPlayer.play("Break")
+		
+
+
+func _on_Area2D_body_exited(body):
+	$Area2D.monitoring == false
+	$Area2D.monitorable == false
+	yield(get_tree().create_timer(6.0), "timeout")
+	$Area2D.monitoring == true
+	$Area2D.monitorable == true
+
+
