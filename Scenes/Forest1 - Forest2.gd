cacdd50bdf96dbb140ef4adae1e302439058ed19
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-11-29 20:18:01 +0000

    Alot of Work
    Forest 2
    Pasue fixes
    camera fixes
    Transitions
    Falling Platfroms

---
+++ Scenes/Forest1 - Forest2.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "Fade Out":
+		get_tree().change_scene("res://Scenes/Forest2.tscn")
+
+
+func _on_Area2D_body_entered(body):
+	if body.get_name() == "Player":
+		$AnimationPlayer.play("Fade Out")
+
